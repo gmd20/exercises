cc0319598372df67280d584e1ae924febbab4a49
gmd20 <gmd20@163.com>
2016-03-22 14:38:23 +0800

    Update bash_debug_args.sh

--- bash_debug_args.sh
+++ bash_debug_args.sh
@@ -6,3 +6,6 @@ tar -czf "${archive_name}" "$@"
 if [ $? -ne 0 ] then
     exit $?
 fi
+
+scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /cfg/my_ssh_known_hosts" -i /cfg/my_ssh_id_rsa ${archive_name} user_name@192.168.1.123:/data || exit $?
+
