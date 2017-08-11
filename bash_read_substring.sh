eaa498c8e6953a5713ad82d5a120cedadd25a211
gmd20 <gmd20@163.com>
2017-08-11 16:37:07 +0800

    Update bash_read_substring.sh

--- bash_read_substring.sh
+++ bash_read_substring.sh
@@ -3,10 +3,10 @@
 while echo "input a key:"
 do
   read key
-  salt="dddd${key:1:1}d${key:6:2}"
+  salt="dddd${key:0:1}d${key:6:2}"
   echo "salt=$salt"
   echo "key=$key"
   digest=`openssl passwd -1 -salt $salt $key`
   echo "$digest"
-  echo "result=${digest:13:22}"
+  echo "result=${digest:12:22}"
 done
