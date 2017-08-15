87d39d6b3c6cea1735294293f6d13444249b0c0b
gmd20 <gmd20@163.com>
2017-08-15 15:31:42 +0800

    Create ssh_expect.pl

---
+++ ssh_expect.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use Expect;
+use strict;
+
+$Expect::Log_Stdout = 1;
+my $cmd  = "";
+my $timeout = 4;
+my $exp ;
+
+$cmd = 'ssh -e \'?\' root@192.168.1.1';
+$exp = Expect->spawn($cmd) or die "Can not spawn $cmd\n";
+
+$exp->expect($timeout,
+  ["password:" => sub {
+      my $exp = shift;
+      $exp->send ("password1\n");
+      exp_continue;
+    }],
+  ["info>" => sub {
+      my $exp = shift;
+      $exp->send("version\n");
+      exp_continue;
+    }],
+  ["Password:" => sub {
+      my $exp = shift;
+      $exp->send("password2\n");
+      exp_continue;
+    } ],
+  [ "root#" => sub {
+      my $exp = shift;
+      $exp->send("\n");
+    }]
+);
+
+$exp->send("ash\n");
+$exp->expect($timeout,
+ '-re', qr/(\w+):/ => sub {
+      my $exp = shift;
+      my $key = ($exp->matchlist)[0];
+      my $salt = $key;
+      substr($salt, 0, 3, "ddd");
+      substr($salt, 5, 2, "bb");
+      my $digest=`openssl passwd -1 -salt $salt $key`;
+      my $result = substr($digest, 12,22);
+      $exp->log_stdout(1);
+      print ("openssl passwd -1 -salt $salt $key\n");
+      print ("digest=$digest\nresult=$result\n");
+      $exp->send("$result\n");
+    }
+);
+
+
+$exp->interact();
+#$exp->soft_close();
+
+
