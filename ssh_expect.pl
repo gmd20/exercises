25505bd90848df468b45c16262b9505a1ef46873
gmd20 <gmd20@163.com>
2019-10-17 18:50:51 +0800

    Update ssh_expect.pl

--- ssh_expect.pl
+++ ssh_expect.pl
@@ -11,6 +11,15 @@ my $exp ;
 $cmd = 'ssh -e \'?\' root@192.168.1.1';
 $exp = Expect->spawn($cmd) or die "Can not spawn $cmd\n";
 
+# I set the terminal size as explained above, but if I resize the window, the application does not notice this.
+# You have to catch the signal WINCH ("window size changed"), change the terminal size and propagate the signal to the spawned application:
+$SIG{WINCH} = \&winch;
+sub winch {
+  $exp->slave->clone_winsize_from(\*STDIN);
+  kill WINCH => $exp->pid if $exp->pid;
+  $SIG{WINCH} = \&winch;
+}
+
 $exp->expect($timeout,
   ["password:" => sub {
       my $exp = shift;
